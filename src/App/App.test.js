f64dc02245eb9c782b5f21cd99d0a1ece56f554b
Allison Wagner <allisonjw01@gmail.com>
2019-12-11 11:33:28 -0700

    Adds test for changing state

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -13,4 +13,18 @@ describe('App', () => {
     expect(wrapper).toMatchSnapshot()
   });
 
+  it('should update state when data is passed in', () => {
+    let mockData = {
+      id: 18907224,
+      name: "Allison",
+      date: "12/5",
+      time: "7:00",
+      number: 6
+    }
+    const expected = [mockData];
+    expect(wrapper.state('reservations')).toEqual([])
+    wrapper.instance().addReservation(mockData);
+    expect(wrapper.state('reservations')).toEqual(expected)
+  });
+
 });
