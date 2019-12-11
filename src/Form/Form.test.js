a9e8c6f38a4d5345cbfab8df73e982035f2c3862
Allison Wagner <allisonjw01@gmail.com>
2019-12-11 11:41:10 -0700

    Adds test for updating state on form

--- src/Form/Form.test.js
+++ src/Form/Form.test.js
@@ -6,11 +6,20 @@ describe('Form', () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<Form />)
+    const mockSubmitReservation = jest.fn()
+    wrapper = shallow(<Form submitReservation={mockSubmitReservation}/>)
   });
 
   it('should match all the data being passed', () => {
     expect(wrapper).toMatchSnapshot()
   });
-  
+
+  it('should update state on handleChange', () => {
+      const mockEvent = {target: {name: 'name', value: 'Allison'}}
+      const expected = 'Allison';
+
+      wrapper.instance().handleChange(mockEvent);
+      expect(wrapper.state('name')).toEqual(expected)
+  })
+
 });
