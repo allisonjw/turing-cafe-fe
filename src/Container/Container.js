58b8b0bfb9668b45291a6409da44ae8ead141cf5
Allison Wagner <allisonjw01@gmail.com>
2019-12-11 09:37:50 -0700

    Adds base of container comp

---
+++ src/Container/Container.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Container.css';
+// import Card from '../Card/Card';
+
+const Container = () => {
+    return (
+        <h2>Hello</h2>
+    )
+}
+
+export default Container;
