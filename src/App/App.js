eec395da336861f5ce20f6cc5194bbf4c3814864
Allison Wagner <allisonjw01@gmail.com>
2019-12-11 09:37:21 -0700

    Imports form and container into App

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import { getReservations } from '../apiCalls';
+import Form from '../Form/Form';
+import Container from '../Container/Container';
 
 class App extends Component {
   constructor() {
@@ -20,10 +22,10 @@ class App extends Component {
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+          <Form />
         </div>
         <div className='resy-container'>
-          
+          <Container />
         </div>
       </div>
     )
