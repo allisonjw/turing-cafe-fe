2d1f1e1e8f789965304b9dd1378bdaea33a0958f
Allison Wagner <allisonjw01@gmail.com>
2019-12-11 10:32:13 -0700

    Adding styling to match comp

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -11,13 +11,41 @@ class Form extends Component {
             number: ''
         }
     }
+
+    handleChange = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
     render() {
         return (
             <form>
-                <input></input>
-                <input></input>
-                <input></input>
-                <input></input>
+                <input
+                  className="form_input"
+                  type="text"
+                  name="name"
+                  value={this.state.name}
+                  onChange={e => this.handleChange(e)}
+                ></input>
+                <input
+                  className="form_input"
+                  type="text"
+                  name="date"
+                  value={this.state.date}
+                  onChange={e => this.handleChange(e)}
+                ></input>
+                <input
+                  className="form_input"
+                  type="text"
+                  name="time"
+                  value={this.state.time}
+                  onChange={e => this.handleChange(e)}
+                ></input>
+                <input
+                  className="form_input"
+                  type="text"
+                  name="number"
+                  value={this.state.number}
+                  onChange={e => this.handleChange(e)}
+                ></input>
                 <button></button>
             </form>
         )
